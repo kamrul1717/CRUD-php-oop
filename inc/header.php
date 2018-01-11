bff7b0e1e0ef59530c8cebea416059ae4819e0be
kamrul <kamrulhassan1361@gmail.com>
2018-01-12 03:14:09 +0600

    simple crud using php oop

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CRUD</title>
+	<style>
+		.tblone{
+			width: 100%;
+			border: 1px solid #fff;
+			margin: 20px 0;
+		}
+		.tblone td{
+			padding: 5px 10px;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
